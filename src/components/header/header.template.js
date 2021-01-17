7dfe405589f0ad469b7ee0fb2e88fb20f7a7df9a
Grigory Telenok <telenok.grigory@gmail.com>
2021-01-17 16:44:51 +1000

    Finish redux and table logic

---
+++ src/components/header/header.template.js
@@ -0,0 +1,21 @@
+import {defaultTitle} from '@/constants';
+
+export function createHeader(state) {
+    return `
+        <input type="text" class="input" value="${state.title || defaultTitle}" />
+
+        <div>
+            <div class="button">
+                <i class="material-icons">
+                    delete
+                </i>
+            </div>
+
+            <div class="button">
+                <i class="material-icons">
+                    exit_to_app
+                </i>
+            </div>
+        </div>
+    `;
+}
